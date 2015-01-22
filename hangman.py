83e97e120f0627821fe63c795caa665bd6a2321f
ldu1225 <ldu1225@naver.com>
2015-01-22 15:31:04 +0900

    changed message again

--- hangman.py
+++ hangman.py
@@ -10,7 +10,7 @@ HANGMANPICS = ['''
       |
       |
       |
-==========''', '''
+*========*''', '''
 
   +---+
   |   |
@@ -18,7 +18,7 @@ HANGMANPICS = ['''
       |
       |
       |
-=========''', '''
+*========*''', '''
 
   +---+
   |   |
@@ -26,7 +26,7 @@ HANGMANPICS = ['''
   |   |
       |
       |
-=========''', '''
+*========*''', '''
 
   +---+
   |   |
@@ -34,7 +34,7 @@ HANGMANPICS = ['''
  /|   |
       |
       |
-=========''', '''
+*========*''', '''
 
   +---+
   |   |
@@ -42,7 +42,7 @@ HANGMANPICS = ['''
  /|\  |
       |
       |
-=========''', '''
+*========*''', '''
 
   +---+
   |   |
@@ -50,7 +50,7 @@ HANGMANPICS = ['''
  /|\  |
  /    |
       |
-=========''', '''
+*========*''', '''
 
   +---+
   |   |
@@ -58,7 +58,7 @@ HANGMANPICS = ['''
  /|\  |
  / \  |
       |
-=========''']
+*========*''']
 words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
 
 def getRandomWord(wordList):
