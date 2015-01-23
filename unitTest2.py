3b578b59509af0527ba15715d05d552eea89a697
ldu1225 <ldu1225@naver.com>
2015-01-23 13:57:01 +0900

    a
    a

--- unitTest2.py
+++ unitTest2.py
@@ -7,25 +7,19 @@ import hangman
 
 class hangmanTestCase(unittest.TestCase):
 
-    def test_CheckCorrectAnswerTrue(self):
+    def test_correct(self):
         answer=hangman.checkCorrectAnswer('tac','cat')
+
         self.assertTrue(answer)
 
-    def test_CheckCorrectAnswerFalse(self):
-        answer = hangman.checkCorrectAnswer('ito', 'cat')
-        self.assertFalse(answer)
-
-    def test_CheckWrongAnswerTrue(self):
-        answer = hangman.checkWrongAnswer('rioooi', 'ourt')
-        self.assertTrue(answer)
-
-<<<<<<< Updated upstream
     def test_CheckWrongAnswerFalse(self):
         answer = hangman.checkWrongAnswer('tac', 'cat')
         self.assertFalse(answer)
-=======
         self.assertEquals(answer,False)
->>>>>>> Stashed changes
+    def test_incorrect(self):
+        answer=hangman.checkCorrectAnswer('ac','cat')
+
+        self.assertEquals(answer,True)
 
 
 if __name__ == '__main__':
